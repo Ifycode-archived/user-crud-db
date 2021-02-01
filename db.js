e0189ef9d9d00414116c790f8fdd0f130381853c
ifycode <marychord@gmail.com>
2021-02-01 03:28:34 -0800

    Package install & Initial setup

---
+++ db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/usersCrudDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+},
+ error => {
+    if (!error) 
+        console.log('MongoDB connection suceeded!!!');
+    else 
+        console.log('Error in DB connection: ' + JSON.stringify(error, undefined, 2));
+});
+
+module.exports = mongoose;
